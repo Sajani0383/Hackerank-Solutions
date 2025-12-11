65083d0b00dc8f7cc3a122d3c53e7b48f6985fcc
Sajani G <sajanichandra0205@gmail.com>
2025-12-11 16:20:45 +0530

    Add files via upload

---
+++ Day12/zipped.py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n,x=map(int,input().split())
+marks=[]
+for _ in range(x):
+    marks.append(list(map(float,input().split())))
+for student in zip(*marks):
+    avg=sum(student)/x
+    print(f"{avg:.1f}")
